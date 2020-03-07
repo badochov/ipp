30c006426f2c76d7514cf4d336f1f7517f8b882e
Hubert Badocha <badocha.hubert@gmail.com>
2020-03-07 13:21:02 +0100

    laby

--- laby/1/issorted.sh
+++ laby/1/issorted.sh
@@ -4,13 +4,15 @@ name=sort
 
 gcc -Wall -Wextra -O2 -o $name sort.c
 
-for f in *.in
+shopt -s nullglob
+for f in tests/*.in
 do
   ./$name <$f >${f%in}out
   echo "Dla pliku $f program zakończył się kodem $?."
 done
 
-for f in *.in
+shopt -s nullglob
+for f in tests/*.in
 do 
 	echo -n "Plik $f "
 	if diff ${f%in}out <(sort -g $f) >/dev/null 2>&1
